461914b71d38ffe0f7bfd843dc58fbec6aee1e46
wamich <391316545@qq.com>
2024-11-05 15:17:07 +0800

    feat: add multi iFrame

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="pug">
+div(class="container")
+  a-input-search(
+    v-model:value="search"
+    placeholder="请输入查询单词"
+    style="display: block; width: 360px; margin: auto;"
+    @search="onSearch"
+  )
+  a-tabs(v-model:activeKey="activeKey")
+    a-tab-pane(v-for="item of dataSource" :key="item.prefix" :tab="item.title")
+      iframe(:src="item.url")
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const DefaultWord = "welcome";
+
+const search = ref<string>(location.pathname.slice(1) || DefaultWord);
+
+const onSearch = () => {
+  console.log(search.value);
+  if (!search.value) search.value = DefaultWord;
+  dataSource.value.forEach((it) => (it.url = it.prefix + "/" + search.value));
+};
+
+const activeKey = ref<string>();
+
+function zero2Local(hostname: string) {
+  return hostname.replace("0.0.0.0", "localhost");
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+const dataSource = ref<
+  {
+    prefix: string;
+    url: string;
+    title: string;
+  }[]
+>([]);
+
+onMounted(async () => {
+  const resp = await fetch("/info", { method: "POST" });
+  const { data } = await resp.json();
+  dataSource.value = data.map((it: any) => {
+    const prefix = "http://" + zero2Local(it.hostname) + ":" + it.port;
+    const url = prefix + "/" + search.value;
+    const title = it.mdxHeader?.Title || it.mdxDir.slice(it.mdxDir.lastIndexOf("/") + 1);
+    return { prefix, url, title };
+  });
+  if (dataSource.value.length) activeKey.value = dataSource.value[0].prefix;
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding: 8px;
+  width: 375px;
+  background-color: rgb(250, 250, 250);
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+iframe {
+  width: 375px;
+  height: max(calc(100vh - 116px), 675px);
+  border: 0;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+  ],
+  server: {
+    proxy: {
+      "/info": "http://localhost:3000",
+    },
+  },
+});
